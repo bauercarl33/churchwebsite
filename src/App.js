0cf5f68f7d77225d46a5b7edff2e7e6f0e119f6d
Carl Bauer <bauercarliii@gmail.com>
2024-08-08 22:38:25 -0500

    general layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Routes,
+} from "react-router-dom";
+import Home from "./pages/homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <h3>Tabs Tabs Tabs Tabs</h3>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,26 @@
+const homepage = () => {
+  return (
+    <div>
+      <h1>Saint Mary's Romanian Orthodox Church</h1>
+      <section>Sliding Pictures</section>
+      <section>
+        <h2>Welcome Message</h2>
+        <p>message lorem ipsum dalore</p>
+        <h3>Who we are</h3>
+        <p>We are a romanian orthodox church founded...</p>
+      </section>
+      <section>
+        <h3>Location: </h3>
+        <p>9218 Balcones Club Dr, Austin Texas</p>
+        <h3>Schedule of Services: </h3>
+        <div>9:00AM Divine Liturgy</div>
+        <a>Google Calendar</a>
+      </section>
+      <section>
+        <h1>High Quality Photos</h1>
+      </section>
+    </div>
+  );
+};
+
+export default homepage;
