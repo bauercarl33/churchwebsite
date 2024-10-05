92ee960214c6d8a1a8da85d30ece39a1c0ee331a
Admin <admin@MacBook-Proface.local>
2024-10-05 13:48:52 -0500

    Fixed iPhone homescreen background image bug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,17 +5,16 @@ import { motion, useScroll, useTransform } from 'framer-motion';
 
 const Home = () => {
     return (
-        
         <div className='home'>
-            <img className='parallax-img' />
-            <div className='content'>
-                <h2>St. Mary</h2>
-                <h4>Orthodox Church</h4>
-                        
+            <div class="e-with-fixed-bg">
+                <div class="bg-wrap">
+                    <div class="bg"></div>
+                </div>
+                <div className='content'>
+                    <h2>St. Mary</h2>
+                    <h4>Orthodox Church</h4>         
+                </div> 
             </div> 
-            {/* <svg className='svg-mask' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 919">
-                <path fill="var(--bg-color)" d="M 0,919 Q 800,919 1920,850 L1920,920 L0,920 Z"></path>
-            </svg>            */}
         </div>
     )
 }
