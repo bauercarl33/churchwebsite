550b9e4e64b0e80ec0fe0e15a6e5ffa686fb9748
andrewhallack <andrewhallack@gmail.com>
2024-10-18 13:01:46 -0500

    Caught bug in calendar

--- src/pages/Calendar/Modal.js
+++ src/pages/Calendar/Modal.js
@@ -7,24 +7,29 @@ const Modal = forwardRef(({ closeModal, events, style, screenWidth }, ref) => {
         <>
             {screenWidth > 600 ?
                 <div className='modal'  ref={ref} style={style}>
-                    <h5>{events.startDate}</h5>
+                    
                     <button className='close-modal' onClick={closeModal}>
                         <FaTimes size={16} />
                     </button>
+                    <h5>{events.startDate}</h5>
                     <h6>{events.summary}</h6>
                     <p>{events.startTime} - {events.endTime}</p>
                 </div> :
                 <div className='modal'>
-                    <h5>{events[0].startDate}</h5>
                     <button className='close-modal' onClick={closeModal}>
                         <FaTimes size={16} />
                     </button>
+                    {events && Array.isArray(events) && <h5>{events[0].startDate}</h5>}
                     {events && Array.isArray(events) && events.map((item, index) => {
                         return (
-                            <div key={index} className='modal-mobile'>
-                                <h6>{item.summary}</h6>
-                                <p>{item.startTime} - {item.endTime}</p>
-                            </div>
+                            <>
+                                
+                                <div key={index} className='modal-mobile'>
+                                    <h6>{item.summary}</h6>
+                                    <p>{item.startTime} - {item.endTime}</p>
+                                </div>
+                            </>
+                            
                         )
                     })}
                 </div>
