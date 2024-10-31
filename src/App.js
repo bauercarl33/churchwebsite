545e041eab27b834bb9316470b098cac1f19894f
Admin <admin@MacBook-Proface.lan>
2024-10-30 21:25:01 -0500

    Added initial visitors section

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Calendar from './pages/Calendar/Calendar'
 import Donate from './pages/Donate/Donate';
 import Footer from "./components/Footer/Footer";
 import Media from "./pages/Media/Media";
+import Visitors from "./pages/Visitors/Visitors";
 
 const queryClient = new QueryClient();
 
@@ -30,6 +31,7 @@ function App() {
             <Route exact path='/calendar' element={<Calendar />} />
             <Route exact path='/media' element={<Media />} />
             <Route path='/media/:name/:id' element={<Media />} />
+            <Route exact path='/visitors' element={<Visitors />} />
             <Route exact path='/donate' element={<Donate />} />
           </Routes>
           <Footer />
---
+++ src/pages/Visitors/QAs.js
@@ -0,0 +1,26 @@
+export const QAs = [
+    {
+        question: "Do I have to be Romanian or Orthodox to attend?",
+        answer: "Dolor in commodo est deserunt tempor."
+    },
+    {
+        question: "What should I expect when I arrive?",
+        answer: "Dolor in commodo est deserunt tempor."
+    },
+    {
+        question: "How should I dress to church?",
+        answer: "Dolor in commodo est deserunt tempor."
+    },
+    {
+        question: "How long is the service?",
+        answer: "Dolor in commodo est deserunt tempor."
+    },
+    {
+        question: "Is there anything after church?",
+        answer: "Dolor in commodo est deserunt tempor."
+    },
+    {
+        question: "Can I receive Communion?",
+        answer: "Dolor in commodo est deserunt tempor."
+    },
+]
