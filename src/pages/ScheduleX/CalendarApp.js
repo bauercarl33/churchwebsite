a92283dc9f25add2ddc10447651c422efc05e046
andrewhallack <andrewhallack@gmail.com>
2024-11-02 15:44:27 -0500

    Fixing calendar, needs some styling

---
+++ src/pages/ScheduleX/CalendarApp.js
@@ -0,0 +1,49 @@
+import { useEffect } from 'react'
+import { useCalendarApp, ScheduleXCalendar } from '@schedule-x/react'
+import { createViewMonthGrid } from '@schedule-x/calendar'
+import { createEventsServicePlugin } from '@schedule-x/events-service'
+import { createEventModalPlugin } from '@schedule-x/event-modal'
+ 
+import '@schedule-x/theme-default/dist/index.css'
+import './calendar-app.css'
+
+ 
+function CalendarApp({ data }) {
+    const plugins = [createEventsServicePlugin(), createEventModalPlugin()]
+
+    const config = {
+        firstDayOfWeek: 0,
+        calendars: {
+            church: {
+                colorName: 'church',
+                lightColors: {
+                    main: 'var(--accent)',
+                    container: 'var(--primary-color)',
+                    onContainer: 'var(--bg-color)',
+                }
+            }
+        }
+        
+    }
+ 
+    useEffect(() => {
+        calendar.eventsService.getAll()
+    }, [])
+
+    const calendar = useCalendarApp({
+        views: [createViewMonthGrid()],
+        events: data,
+        firstDayOfWeek: config.firstDayOfWeek,
+        calendars: config.calendars
+    }, plugins)
+
+    return (
+        <div className='calendar'>
+            <ScheduleXCalendar 
+                calendarApp={calendar}
+            />
+        </div>
+    )
+}
+ 
+export default CalendarApp
