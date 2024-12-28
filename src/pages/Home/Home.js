99ef277f120bbed646690e3e49bd8e679a955384
Carl Bauer <bauercarliii@gmail.com>
2024-12-27 20:43:10 -0600

    added decorative strip for home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import Hero from './Hero/Hero';
-import ColSection from '../../components/ColSection/ColSection';
-import Welcome from './Welcome/Welcome';
-import Mission from './Mission/Mission';
+import Hero from "./Hero/Hero";
+import ColSection from "../../components/ColSection/ColSection";
+import Welcome from "./Welcome/Welcome";
+import Mission from "./Mission/Mission";
 
-import { colSectionVars } from './ColSectionVars';
+import { colSectionVars } from "./ColSectionVars";
 
 const Home = () => {
-    return (
-        <>
-            <Hero />
-            <Welcome />
-            <ColSection colSectionVars={colSectionVars} /> 
-            <Mission />           
-        </>
-    )
-}
+  return (
+    <>
+      <Hero />
 
-export default Home
+      <Welcome />
+
+      <img
+        src="https://saintmarychurch.s3.us-east-1.amazonaws.com/images/1zZv2ycw36laHYoHQAz3A9lL6W4CUeGFI"
+        style={{ width: 2000, paddingTop: 7 }}
+      />
+      <ColSection colSectionVars={colSectionVars} />
+      <Mission />
+
+      <img
+        src="https://saintmarychurch.s3.us-east-1.amazonaws.com/images/1zZv2ycw36laHYoHQAz3A9lL6W4CUeGFI"
+        style={{ width: 2000, paddingTop: 7 }}
+      />
+    </>
+  );
+};
+
+export default Home;
