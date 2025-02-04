528fee13f270de4f665db8bd40f72ca7d0c74433
Carl Bauer <bauercarliii@gmail.com>
2025-02-04 02:19:04 -0600

    added vespers

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -1,170 +1,184 @@
-import { useState, useEffect } from 'react';
-import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
-import { useQuery } from '@tanstack/react-query';
+import { useState, useEffect } from "react";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { useQuery } from "@tanstack/react-query";
 
-import './calendar.css';
-import MonthView from './MonthView';
-import { months } from './CalVars';
+import "./calendar.css";
+import MonthView from "./MonthView";
+import { months } from "./CalVars";
 
-const url = "https://ritymdmzg4.execute-api.us-east-1.amazonaws.com/prod/getChurchCalendar";
+const url =
+  "https://ritymdmzg4.execute-api.us-east-1.amazonaws.com/prod/getChurchCalendar";
 
 const Calendar = () => {
-    const [timeFrame, setTimeFrame] = useState('month');
-    const [calendarDays, setCalendarDays] = useState([]);
-    const [offset, setOffset] = useState(0);
+  const [timeFrame, setTimeFrame] = useState("month");
+  const [calendarDays, setCalendarDays] = useState([]);
+  const [offset, setOffset] = useState(0);
 
-    const fetchCalendarData = async () => {
-        const year = new Date().getFullYear();
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                startDate: `${year}-01-01`,
-                endDate: `${year + 1}-12-31`,
-            }),
-        });
-        if (!response.ok) {
-            throw new Error(`STATUS: ${response.status}`);
-        }
-        const data = await response.json();
-        return formatCalendarJson(data.body);
-    };
-
-    const { data: formattedData, isLoading, isError, error } = useQuery({
-        queryKey: ['calendarData'],
-        queryFn: fetchCalendarData,
+  const fetchCalendarData = async () => {
+    const year = new Date().getFullYear();
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        startDate: `${year}-01-01`,
+        endDate: `${year + 1}-12-31`,
+      }),
     });
-
-    const formatCalendarJson = (json) => {
-        const newJson = {};
-        json.forEach((item) => {
-            const { summary, location, start, end } = item;
-            const startDate = new Date(start.dateTime);
-            const endDate = new Date(end.dateTime);
-
-            const yy = startDate.getFullYear();
-            const mm = startDate.getMonth();
-            const dd = startDate.getDate();
-            
-            const eventDetails = {
-                startDate: startDate.toLocaleDateString('en-US', {
-                    year: 'numeric',
-                    month: 'short',
-                    day: 'numeric'
-                }),
-                startTime: startDate.toLocaleTimeString([], {
-                    hour: '2-digit',
-                    minute: '2-digit'
-                }),
-                endDate: endDate,
-                endTime: endDate.toLocaleTimeString([], {
-                    hour: '2-digit',
-                    minute: '2-digit'
-                }),
-                timezone: "America/Chicago",
-                summary: summary,
-                location: location,
-            };
-
-            newJson[yy] ??= {};
-            newJson[yy][mm] ??= {};
-            newJson[yy][mm][dd] ??= { events: [] };
-            newJson[yy][mm][dd].events.push(eventDetails);
-        });
-        return newJson;
-    };
-
-    const handleCalendarDays = (mOffset = 0) => {
-        const date = new Date();
-        const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth() + mOffset, 1);
-        const weekdayOfFirstDay = firstDayOfMonth.getDay();
-        let currentDays = [];
-        const totalDays = 42;
-
-        for (let day = 0; day < totalDays; day++) {
-            if (day === 0 && weekdayOfFirstDay === 0) {
-                firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 7);
-            } else if (day === 0) {
-                firstDayOfMonth.setDate(firstDayOfMonth.getDate() + (day - weekdayOfFirstDay));
-            } else {
-                firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
-            }
-
-            const calendarDay = {
-                currentMonth: firstDayOfMonth.getMonth() === date.getMonth(),
-                date: new Date(firstDayOfMonth),
-                month: firstDayOfMonth.getMonth(),
-                day: firstDayOfMonth.getDate(),
-                selected: firstDayOfMonth.toDateString() === date.toDateString(),
-                year: firstDayOfMonth.getFullYear(),
-            };
-
-            currentDays.push(calendarDay);
-        }
-
-        if (currentDays[6].month !== currentDays[15].month) {
-            currentDays = currentDays.slice(7);
-        }      
-        if (currentDays[currentDays.length - 7].month !== currentDays[15].month) {
-            currentDays = currentDays.slice(0, -7);
-        }
-
-        setCalendarDays(currentDays);
-    };
-
-    const handleToday = () => {
-        setOffset(0);
-        handleCalendarDays(0);
-    };
-
-    const handlePreviousMonth = () => {
-        const newOffset = offset - 1;
-        setOffset(newOffset);
-        handleCalendarDays(newOffset);
-    };
-
-    const handleNextMonth = () => {
-        const newOffset = offset + 1;
-        setOffset(newOffset);
-        handleCalendarDays(newOffset);
-    };
-
-    useEffect(() => {
-        handleCalendarDays(offset);
-    }, [offset]);
-
-    if (isLoading) {
-        return <div className='calendar'>Loading...</div>;
+    if (!response.ok) {
+      throw new Error(`STATUS: ${response.status}`);
     }
-    if (isError) {
-        return <div className='calendar'>Error: {error.message}</div>;
-    }
+    const data = await response.json();
+    return formatCalendarJson(data.body);
+  };
 
-    return (
-        <div className='calendar'>
-            <div className='container'>
-                <div className='head'>
-                    <h5 id='cal-title'>
-                        {calendarDays.length > 15 ? `${months[calendarDays[15].month]} ${calendarDays[15].year}` : 'Loading...'}
-                    </h5>
-                    <div className='buttons'>
-                        <button className='button' onClick={handlePreviousMonth}>
-                            <FaAngleLeft />
-                        </button>
-                        <button className='button' onClick={handleToday}>
-                            Today
-                        </button>
-                        <button className='button' onClick={handleNextMonth}>
-                            <FaAngleRight />
-                        </button>
-                    </div>
-                </div>
-                <MonthView calendarDays={calendarDays} formattedData={formattedData} />
-            </div>
-        </div>
+  const {
+    data: formattedData,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["calendarData"],
+    queryFn: fetchCalendarData,
+  });
+
+  const formatCalendarJson = (json) => {
+    const newJson = {};
+    json.forEach((item) => {
+      const { summary, location, start, end } = item;
+      const startDate = new Date(start.dateTime);
+      const endDate = new Date(end.dateTime);
+
+      const yy = startDate.getFullYear();
+      const mm = startDate.getMonth();
+      const dd = startDate.getDate();
+
+      const eventDetails = {
+        startDate: startDate.toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        }),
+        startTime: startDate.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+        endDate: endDate,
+        endTime: endDate.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+        timezone: "America/Chicago",
+        summary: summary,
+        location: location,
+      };
+
+      newJson[yy] ??= {};
+      newJson[yy][mm] ??= {};
+      newJson[yy][mm][dd] ??= { events: [] };
+      newJson[yy][mm][dd].events.push(eventDetails);
+    });
+    return newJson;
+  };
+
+  const handleCalendarDays = (mOffset = 0) => {
+    const date = new Date();
+    const firstDayOfMonth = new Date(
+      date.getFullYear(),
+      date.getMonth() + mOffset,
+      1
     );
+    const weekdayOfFirstDay = firstDayOfMonth.getDay();
+    let currentDays = [];
+    const totalDays = 42;
+
+    for (let day = 0; day < totalDays; day++) {
+      if (day === 0 && weekdayOfFirstDay === 0) {
+        firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 7);
+      } else if (day === 0) {
+        firstDayOfMonth.setDate(
+          firstDayOfMonth.getDate() + (day - weekdayOfFirstDay)
+        );
+      } else {
+        firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
+      }
+
+      const calendarDay = {
+        currentMonth: firstDayOfMonth.getMonth() === date.getMonth(),
+        date: new Date(firstDayOfMonth),
+        month: firstDayOfMonth.getMonth(),
+        day: firstDayOfMonth.getDate(),
+        selected: firstDayOfMonth.toDateString() === date.toDateString(),
+        year: firstDayOfMonth.getFullYear(),
+      };
+
+      currentDays.push(calendarDay);
+    }
+
+    if (currentDays[6].month !== currentDays[15].month) {
+      currentDays = currentDays.slice(7);
+    }
+    if (currentDays[currentDays.length - 7].month !== currentDays[15].month) {
+      currentDays = currentDays.slice(0, -7);
+    }
+
+    setCalendarDays(currentDays);
+  };
+
+  const handleToday = () => {
+    setOffset(0);
+    handleCalendarDays(0);
+  };
+
+  const handlePreviousMonth = () => {
+    const newOffset = offset - 1;
+    setOffset(newOffset);
+    handleCalendarDays(newOffset);
+  };
+
+  const handleNextMonth = () => {
+    const newOffset = offset + 1;
+    setOffset(newOffset);
+    handleCalendarDays(newOffset);
+  };
+
+  useEffect(() => {
+    handleCalendarDays(offset);
+  }, [offset]);
+
+  if (isLoading) {
+    return <div className="calendar">Loading...</div>;
+  }
+  if (isError) {
+    return <div className="calendar">Error: {error.message}</div>;
+  }
+
+  return (
+    <div className="calendar">
+      <div className="container">
+        <div className="head">
+          <h5 id="cal-title">
+            {calendarDays.length > 15
+              ? `${months[calendarDays[15].month]} ${calendarDays[15].year}`
+              : "Loading..."}
+          </h5>
+          <div className="buttons">
+            <button className="button" onClick={handlePreviousMonth}>
+              <FaAngleLeft />
+            </button>
+            <button className="button" onClick={handleToday}>
+              Today
+            </button>
+            <button className="button" onClick={handleNextMonth}>
+              <FaAngleRight />
+            </button>
+          </div>
+        </div>
+        <MonthView calendarDays={calendarDays} formattedData={formattedData} />
+      </div>
+    </div>
+  );
 };
 
 export default Calendar;
--- src/pages/Home/Welcome/Welcome.js
+++ src/pages/Home/Welcome/Welcome.js
@@ -1,47 +1,60 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './welcome.css'
-import { FaChevronRight } from 'react-icons/fa6'
+import "./welcome.css";
+import { FaChevronRight } from "react-icons/fa6";
 
 const Welcome = () => {
   return (
-    <div className='welcome'>
-        <div className='container'>
-            <div className='card'>
-                <h5>Temporary Location</h5>
-                <p>13740 Research Boulevard, 1 Lake Creek Office Park Suite W, Austin, TX 78759</p>
-                <a 
-                    href='https://maps.app.goo.gl/emh2KpYtpmYaEc4o8'
-                    aria-label='Map to church'
-                    target='_blank'
-                >
-                    Map<span><FaChevronRight size={12} /></span>
-                </a>
-            </div>
-            <div className='card'>
-                <h5>Divine Services</h5>
-                <p>Orthros on Sundays at 9:30am followed by the Divine Liturgy of St. John Chrysostom at 10:30am. Check the calendar for more.</p>
-                <Link
-                    to='/calendar' 
-                    aria-label='Church calendar'
-                >
-                    Calendar<span><FaChevronRight size={12} /></span>
-                </Link>
-            </div>
-            <div className='card'>
-                <h5>Visitors Welcome</h5>
-                <p>Please come, we love visitors! If you have any questions, please see the FAQ.</p>
-                <Link
-                    to='/visitors' 
-                    aria-label='Frequently asked questions'
-                >
-                    Visitors<span><FaChevronRight size={12} /></span>
-                </Link>
-            </div>
+    <div className="welcome">
+      <div className="container">
+        <div className="card">
+          <h5>Temporary Location</h5>
+          <p>
+            13740 Research Boulevard, 1 Lake Creek Office Park Suite W, Austin,
+            TX 78759
+          </p>
+          <a
+            href="https://maps.app.goo.gl/emh2KpYtpmYaEc4o8"
+            aria-label="Map to church"
+            target="_blank"
+          >
+            Map
+            <span>
+              <FaChevronRight size={12} />
+            </span>
+          </a>
         </div>
+        <div className="card">
+          <h5>Divine Services</h5>
+          <p>
+            Vespers at 4pm every Saturday. Orthros on Sundays at 9:30am followed
+            by the Divine Liturgy of St. John Chrysostom at 10:30am. Check the
+            calendar for more.
+          </p>
+          <Link to="/calendar" aria-label="Church calendar">
+            Calendar
+            <span>
+              <FaChevronRight size={12} />
+            </span>
+          </Link>
+        </div>
+        <div className="card">
+          <h5>Visitors Welcome</h5>
+          <p>
+            Please come, we love visitors! If you have any questions, please see
+            the FAQ.
+          </p>
+          <Link to="/visitors" aria-label="Frequently asked questions">
+            Visitors
+            <span>
+              <FaChevronRight size={12} />
+            </span>
+          </Link>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
