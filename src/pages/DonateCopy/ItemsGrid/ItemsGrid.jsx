17acf55e24167eda572bc2dbe5172dd40aeda5a6
Carl Bauer <bauercarliii@gmail.com>
2025-05-10 21:49:23 -0500

    added quantity box and fixed progress bar

--- src/pages/DonateCopy/ItemsGrid/ItemsGrid.jsx
+++ src/pages/DonateCopy/ItemsGrid/ItemsGrid.jsx
@@ -17,24 +17,40 @@ const ItemsGrid = ({ items, category }) => {
       setLocalCategory(category);
     }
   }, [category]);
+
+  const onCardClick = (item) => {
+    console.log("item clicked!");
+    console.log(item);
+  };
   return (
     <div className="items-container">
       {localCategory == "All"
         ? localItems.map((item) => (
-            <div className="item-card" key={item.itemName}>
-              <img className="item-img" src={item.imgUrl} />
+            <div
+              className="item-card"
+              key={item.itemName}
+              onClick={() => onCardClick(item)}
+            >
+              <div className="image-wrapper">
+                <img
+                  className="item-img"
+                  src={item.imgUrl}
+                  alt={item.itemName}
+                />
+                {5 > 1 && <div className="item-quantity">{`x30`}</div>}
+              </div>
               <div className="title">Hand Engraved Chair </div>
               <div className="progress-row">
-                <div
-                  className="progress-bar"
-                  style={{
-                    "--progress": `${(item.progress / item.cost) * 100}%`,
-                  }}
-                />
                 <div className="item-remaining">{`$${
                   item.cost - item.progress
                 } left!`}</div>
               </div>
+              <div
+                className="progress-bar"
+                style={{
+                  "--progress": `${(item.progress / item.cost) * 100}%`,
+                }}
+              />
             </div>
           ))
         : localItems
