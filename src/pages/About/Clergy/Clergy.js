dbce96e7fe96424f2bab4c931d5e174dea3fb159
andrewhallack <andrewhallack@gmail.com>
2025-10-29 19:27:25 -0500

    Removed birthdate

--- src/pages/About/Clergy/Clergy.js
+++ src/pages/About/Clergy/Clergy.js
@@ -33,7 +33,7 @@ const Clergy = () => {
             <div className='col'>
                 <h4>Our Clergy</h4>
                 <p>
-                    Our current priest, Fr. Gabriel-Teofil Ilies, was born in Romania on October 31, 1988. He completed his studies at the Orthodox Seminary Venianim Costachi of Neamt Monastery, and continued at Fr. Dumitru Staniloae Orthodox University in Iasi. In 2013, Fr. Gabriel moved to the United States and was ordained as a Deacon on March 1, 2020. In February 26, 2022, he had become the Priest of Saint Mary Romanian Orthodox Church. Fr. Gabriel and his wife, Ana-Ioana Ilies, are blessed with two children, Timotei and Elena.
+                    Our current priest, Fr. Gabriel-Teofil Ilies completed his studies at the Orthodox Seminary Venianim Costachi of Neamt Monastery, and continued at Fr. Dumitru Staniloae Orthodox University in Iasi. In 2013, Fr. Gabriel moved to the United States and was ordained as a Deacon on March 1, 2020. In February 26, 2022, he had become the Priest of Saint Mary Romanian Orthodox Church. Fr. Gabriel and his wife, Ana-Ioana Ilies, are blessed with two children, Timotei and Elena.
                 </p>
             </div>
             <div className='col'>
